4414d859fd7be9fbc89850cfd1c0ed2a24e6b634
OishiiCha <86476845+OishiiCha@users.noreply.github.com>
2022-09-05 18:47:29 +0100

    Update wifiqr.py

--- wifiqr.py
+++ wifiqr.py
@@ -1,11 +1,11 @@
 import qrcode
-import pyperclip as pc
 import time
 import os
 import platform
 from datetime import datetime
 import sys
 import subprocess
+import io
 
 # Static
 ans = 1
@@ -23,6 +23,14 @@ def openimage(path):
                                   'darwin':'open'}[sys.platform]
     subprocess.run([imageViewerFromCommandLine, path])
 
+def terminal_qr(mqr):
+    qr = qrcode.QRCode()
+    qr.add_data(mqr)
+    h = io.StringIO()
+    qr.print_ascii(out=h)
+    h.seek(0)
+    print(h.read())
+
 def make_qr():
     date = datetime.now().strftime('%Y%m%d_%H-%M-%S')
     ssid = str(input('SSID: '))
@@ -33,6 +41,7 @@ def make_qr():
     type(img)
     savestr = str('export/'+filename)
     img.save(savestr)
+    terminal_qr(savestr)
     time.sleep(0.1)
     op = input('Would you like to open the Generated QR Code? (Y,N): ')
     f_op = op.upper().replace(' ','')
@@ -58,7 +67,7 @@ def loop_func():
         ans = 1
     else:
         ans = 0
-        clear()
+
 
 def run():
     title()
@@ -67,5 +76,7 @@ def run():
     loop_func()
         
 if __name__ == '__main__':
+    
     while ans == 1:
         run()
+        
